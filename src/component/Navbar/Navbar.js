5bdf02ffa294f9eb2e6546fb03d8f4a5594684fd
Rohit Gautam <rohitgautamofficialmail@gmail.com>
2023-12-03 17:56:08 +0530

    Navbar scroll bug fix

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import './Navbar.css';
 import MenuItems from "./MenuItems";
 import NearMe from "./svg/NearMe.svg";
@@ -7,6 +7,30 @@ import AboutUs from "./svg/AboutUs.svg";
 import Scope from './svg/Scope.svg'
 
 class Navbar extends Component {
+        constructor(props){
+            super(props);
+            this.state = {
+                scrolling : false,
+            };
+        }
+
+        componentDidMount(){
+            window.addEventListener('scroll', this.handleScroll);
+        }
+
+        componentWillUnmount(){
+            window.addEventListener('scroll', this.handleScroll);
+        }
+
+        handleScroll  = () => {
+            if(window.scrollY > 0){
+                this.setState({scrolling : true})
+            }else{
+                this.setState({scrolling: false})
+            }
+        }
+    
+
     renderSvg(iconName) {
         if (iconName === 'NearMe') {
             return NearMe;
@@ -20,10 +44,12 @@ class Navbar extends Component {
     }
 
     render() {
+        const { scrolling } = this.state;
+        const navbarBackground = scrolling ? '#003C40' : 'transparent';
         return (
             <>
                 <div className="header"></div>
-                <nav className="w-full px-4 py-2 text-sm fixed top-5 left-1/2 transform -translate-x-1/2 text-white">
+                <nav style={{ backgroundColor: navbarBackground , zIndex: 100 }} className="w-full px-4 py-4 text-sm fixed top-0 left-1/2 transform -translate-x-1/2 text-white">
                     <div className="container mx-auto">
                         <div className="flex items-center justify-between">
                             <h1 href="#" className="text-gray-600 text-3xl font-bold cursor-pointer"></h1>
